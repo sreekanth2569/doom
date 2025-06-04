45594abf026b7a70eb1b6d017dee0938d5211c46
sreekanth avuleti <sreekanthavuleti@gmail.com>
2025-06-04 12:07:26 +0530

    lol

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
-const Header = () => {
+// Function to generate the list
+const getListOfItems = () => {
   return (
-    <div className="header">
-        <div className="header-contents">
-            <h2>Order your favorite food here</h2>
-            <p>Choose from a deverse menu featuring a delectable array of dishes crafted with the food</p>
-            <button>View Menu</button>
-        </div>
+    <div>
+      <ul>
+        <li>Oranges</li>
+        <li>Banana</li>
+        <li>Fish</li>
+        <li>Dog</li>
+        <li>Coco</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+const Header = () => {
+  const [showList, setShowList] = useState(false); // toggle state
+
+  const handleToggle = () => {
+    setShowList(prev => !prev); // toggle true/false
+  };
+
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order your favorite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the food
+        </p>
+        <button onClick={handleToggle}>
+          {showList ? 'Hide Menu' : 'View Menu'}
+        </button>
+
+        {/* Show list if state is true */}
+        {showList && getListOfItems()}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
